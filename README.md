f4aaacdb9c7d5d9d0f4f6207364144323b5f42a1
Chris Johnson <chrisjohnson00@hotmail.com>
2021-12-19 15:58:31 -0800

    feat: migrate to pulsar for messaging (#2)
    * feat: migrating to pulsar
    
    BREAKING CHANGE: Moving to pulsar
    
    * ci: adding github actions, removing travis
    
    * chore: adding deployment.yaml
    
    * chore: adding deployment.yaml
    
    * chore: updating requirements.txt
    
    * chore: updating requirements.txt
    
    * refactor: moving move type to env var
    
    * chore: fixing indentation
    chore: adding .dockerignore
    
    * chore: fixing yamllint

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # reprocess-files
+
+# PyPi Dependencies
+
+    pip install --upgrade pip
+    pip install --upgrade pulsar-client fastavro pygogo
+    pip freeze > requirements.txt
+    sed -i '/pkg_resources/d' requirements.txt
+
+# Running
+
+    kubectl apply -f kubernetes/job.yaml
--- app.py
+++ app.py
@@ -1,16 +1,34 @@
 from json import dumps
-from kafka import KafkaProducer
+import pulsar
 import os
+import pygogo as gogo
 
-producer = KafkaProducer(bootstrap_servers=['kafka-headless.kafka.svc.cluster.local:9092'],
-                         acks=1,
-                         value_serializer=lambda x:
-                         dumps(x).encode('utf-8'))
+# logging setup
+kwargs = {}
+formatter = gogo.formatters.structured_formatter
+logger = gogo.Gogo('struct', low_formatter=formatter).get_logger(**kwargs)
 
-move_type = "tv"
-directory = os.listdir("/output")
-for filename in directory:
-    message = {'filename': filename, 'move_type': move_type}
-    print("Sending message {}".format(message), flush=True)
-    future = producer.send(topic='handbrakeFile', value=message)
-    result = future.get(timeout=60)
+
+def send_completion_message(file_name, move_type_name):
+    pulsar_server = get_config('PULSAR_SERVER')
+    pulsar_topic = get_config('PULSAR_TOPIC')
+    if pulsar_server and pulsar_topic:
+        client = pulsar.Client(f"pulsar://{pulsar_server}")
+        producer = client.create_producer(pulsar_topic)
+        message = {'filename': file_name, 'move_type': move_type_name}
+        producer.send(dumps(message).encode('utf-8'))
+        logger.info("Notification sent", extra={'message_body': message, 'topic': pulsar_topic, 'file': file_name})
+        client.close()
+    else:
+        logger.warning("PULSAR_SERVER or PULSAR_TOPIC was not found in configs, no messages will be sent")
+
+
+def get_config(key):
+    if os.environ.get(key):
+        return os.environ.get(key)
+
+
+if __name__ == "__main__":
+    directory = os.listdir("/output")
+    for filename in directory:
+        send_completion_message(filename, get_config('MOVE_TYPE'))
